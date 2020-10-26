56f03262f786f48ec91d668ac301faded56b129a
CiberAdventureCat <kbatwoma@student.21-school.ru>
2020-10-26 16:46:34 +0300

    h

---
+++ srcs/nginx/start.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+# config nginx
+mv /tmp/your_domain /etc/nginx/conf.d/default.conf
+
+# ssl
+openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /etc/ssl/private/nginx-selfsigned.key -out /etc/ssl/certs/nginx-selfsigned.crt -subj "/C=RU/ST=Moscow/L=Moscow/O=School21/CN=kbatwoma"
+
+# start
+nginx -g 'daemon off;'
