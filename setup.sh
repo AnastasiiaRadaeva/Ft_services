cbc22ba2db5e3f635daaab22f32883c564a21158
CiberAdventureCat <kbatwoma@student.21-school.ru>
2020-10-27 17:28:26 +0300

    gg

--- setup.sh
+++ setup.sh
@@ -1,2 +1,13 @@
-#This script will setup
-#all your applications.
+minikube start --vm-driver=virtualbox
+
+eval $(minikube docker-env)
+
+# Make metallb
+minikube addons enable metallb
+kubectl apply -f srcs/metallb_config.yaml
+
+# Containers
+docker build -t nginx_image ./srcs/
+
+# Create files .yaml
+kubectl apply -f srcs/nginx/nginx.yaml
--- srcs/nginx/start.sh
+++ srcs/nginx/start.sh
@@ -7,4 +7,4 @@ mv /tmp/your_domain /etc/nginx/conf.d/default.conf
 openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /etc/ssl/private/nginx-selfsigned.key -out /etc/ssl/certs/nginx-selfsigned.crt -subj "/C=RU/ST=Moscow/L=Moscow/O=School21/CN=kbatwoma"
 
 # start
-nginx -g 'daemon off;'
+nginx -g 'daemon off;' #запускаем без демона, в фоновом режиме???
