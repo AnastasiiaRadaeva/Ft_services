7bd2bf7db289467fbdb7eb385ee0c51ecdc6ab9e
CiberAdventureCat <kbatwoma@student.21-school.ru>
2020-10-28 14:41:30 +0300

    j

--- setup.sh
+++ setup.sh
@@ -2,12 +2,14 @@ minikube start --vm-driver=virtualbox
 
 eval $(minikube docker-env)
 
+minikube dashboard & #открываем дашборд на заднем фоне
+
 # Make metallb
 minikube addons enable metallb
 kubectl apply -f srcs/metallb_config.yaml
 
 # Containers
-docker build -t nginx_image ./srcs/
+docker build -t nginx_image ./srcs/nginx
 
 # Create files .yaml
 kubectl apply -f srcs/nginx/nginx.yaml
