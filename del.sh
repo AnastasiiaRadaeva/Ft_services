c55715052df7b7203607633187552bbc0520c8da
CiberAdventureCat <kbatwoma@student.21-school.ru>
2020-11-02 17:09:48 +0300

    tt

--- del.sh
+++ del.sh
@@ -4,5 +4,14 @@ kubectl delete service nginx-svc
 kubectl delete deployment ftps-deployment
 kubectl delete service ftps-svc
 
+kubectl delete deployment php-deployment
+kubectl delete service php-svc
+
+kubectl delete deployment mysql-deployment
+kubectl delete service mysql-svc
+
+kubectl delete deployment wordpress-deployment
+kubectl delete service wordpress-svc
+
 # minikube stop
 # minikube delete
--- setup.sh
+++ setup.sh
@@ -8,10 +8,18 @@ minikube dashboard & #открываем дашборд на заднем фон
 minikube addons enable metallb
 kubectl apply -f srcs/metallb_config.yaml
 
+kubectl apply -f ./srcs/volume.yaml
+
 # Containers
 docker build -t nginx_image ./srcs/nginx
 docker build -t ftps_image ./srcs/FTPS
+docker build -t mysql_image ./srcs/MySQL
+docker build -t php_image ./srcs/phpMyAdmin
+docker build -t wordpress_image ./srcs/WordPress
 
 # Create files .yaml
 kubectl apply -f srcs/nginx/nginx.yaml
 kubectl apply -f srcs/FTPS/ftps.yaml
+kubectl apply -f srcs/MySQL/mysql.yaml
+kubectl apply -f srcs/phpMyAdmin/php.yaml
+kubectl apply -f srcs/WordPress/wordpress.yaml
